ac9ecc6ab8aebf4a5d9d88d9060e0f9d485929e4
Tadeáš <hello.dev@tadeasthelen.cz>
2025-10-12 20:16:13 +0200

    Update front-page layout and enhance styling
    - Added new image wrapper and image elements to front-page.twig for improved visual presentation.
    - Updated tech stack in front-page.php to include Svelte, TypeScript, and a CSS framework.
    - Introduced new CSS styles for the image wrapper and image to enhance responsiveness and aesthetics.

--- front-page.php
+++ front-page.php
@@ -6,6 +6,6 @@
 $context = Timber::context();
 $context['theme_name'] = 'Core Theme';
 $context['description'] = 'This is modern WordPress theme built with';
-$context['tech_stack'] = ['PHP', 'Timber', 'Twig', 'Vite'];
+$context['tech_stack'] = ['PHP', 'Timber', 'Twig', 'Vite', 'Svelte', 'TypeScript', 'CSS framework'];
 
 Timber::render('pages/front-page.twig', $context);
